871fd8c6943b8d4ed07f5fc8aa0b1cf2c279cc76
Davi Quirino <davipires7@hotmail.com>
2022-06-16 21:14:00 -0300

    creating routes

--- Ailos/src/app/app.module.ts
+++ Ailos/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { HeaderComponent } from './Core/header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { ConsultComponent } from './consult/consult.component';
 import { NavListComponent } from './Core/nav-list/nav-list.component';
+import { UserInfosComponent } from './user-infos/user-infos.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ConsultComponent, NavListComponent],
+  declarations: [AppComponent, HeaderComponent, ConsultComponent, NavListComponent, UserInfosComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
